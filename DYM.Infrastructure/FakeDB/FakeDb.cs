df306a5b108182a9a210cfe876a010b8d862c06d
Whiitetesla <whiite.h.l@gmail.com>
2019-03-27 09:51:51 +0100

    added Structure for unitOfWork

---
+++ DYM.Infrastructure/FakeDB/FakeDb.cs
@@ -0,0 +1,22 @@
+﻿using DYM.Doman.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DYM.Infrastructure.FakeDB
+{
+    public class FakeDb
+    {
+        public List<Entity> Words { get; private set; }
+
+        public FakeDb()
+        {
+            FillDB();
+        }
+
+        private void FillDB()
+        {
+            Words = new List<Entity>();
+        }
+    }
+}
