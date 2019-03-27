9f4a84aab6277b66e2670ded28a709a8dbe1200c
Jonas Laursen <joamla96@gmail.com>
2019-03-27 09:34:27 +0100

    Added interface from contractor

---
+++ DYM.Doman/Interfaces/IDYMApi.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DYM.Doman.Interfaces {
+	interface IDYMApi {
+		List<string> DidYouMean(string query, int size);
+	}
+}
